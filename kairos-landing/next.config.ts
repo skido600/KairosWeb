9d70366b66932c0b3aad47266df683152ea18e8d
TreasureUzoma <treasureuzoma650@gmail.com>
2025-04-11 12:59:14 +0100

    did multizone

--- kairos-landing/next.config.ts
+++ kairos-landing/next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "kairosapp.vercel.app",
+                pathname: "/**"
+            }
+        ]
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/home",
+                destination: "https://kairosapp.vercel.app"
+            },
+            {
+                // Catch-all route for any other blog paths
+                source: "/:path+",
+                destination: "https://kairosapp.vercel.app/:path+"
+            },
+            {
+                source: "/app/_next/:path+",
+                destination:
+                    "https://kairosapp.vercel.app/app/_next/:path+"
+            }
+        ];
+    }
 };
 
 export default nextConfig;
