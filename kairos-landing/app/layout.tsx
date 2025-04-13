939c1a531ed9127593b869d69006b88f2a63799f
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-04-13 09:18:42 +0100

    restore the default layout set

--- kairos-landing/app/layout.tsx
+++ kairos-landing/app/layout.tsx
@@ -1,38 +1,32 @@
 import type { Metadata } from "next";
-import { Geist_Mono, DM_Sans } from "next/font/google";
-import clsx from "clsx";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const dmSans = DM_Sans({
-  // variable: "--font-geist-sans",
-  subsets: ["latin"],
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"]
 });
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"]
+});
 
 export const metadata: Metadata = {
   title: "Kairos - Keep Streaks",
   description:
     "Kairos is a intuitive dev community that allows you create project, keep streaks, get reviews with a leaderboard feature.",
-  keywords:
-    "kairos, streak, 30 days challenge, #30daysofcide, reddit, vickyjaychallenge",
+  keywords: "kairos, streak, 30 days challenge, #30daysofcide, reddit, vickyjaychallenge"
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={clsx(dmSans.className, "antialiased")}
-      >
-        {children}
-      </body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
