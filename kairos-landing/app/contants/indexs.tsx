9f99da1e24c4192985a97c3d14351dcff03819ad
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-04-16 18:20:47 +0100

    about

--- kairos-landing/app/contants/indexs.tsx
+++ kairos-landing/app/contants/indexs.tsx
@@ -208,44 +208,6 @@ export const teams = [
   },
 ];
 
-export const logos = [
-  {
-    id: "0",
-    title: "Afterpay",
-    url: "/images/logos/afterpay.svg",
-    width: 156,
-    height: 48,
-  },
-  {
-    id: "1",
-    title: "Amplitude",
-    url: "/images/logos/amplitude.svg",
-    width: 194,
-    height: 48,
-  },
-  {
-    id: "2",
-    title: "Sonos",
-    url: "/images/logos/sonos.svg",
-    width: 115,
-    height: 48,
-  },
-  {
-    id: "3",
-    title: "Maze",
-    url: "/images/logos/maze.svg",
-    width: 142,
-    height: 48,
-  },
-  {
-    id: "4",
-    title: "Drips",
-    url: "/images/logos/drips.svg",
-    width: 77,
-    height: 48,
-  },
-];
-
 
 
 export const socials = [
