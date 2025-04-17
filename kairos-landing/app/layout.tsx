ad45fd0dc9d0b704ad16a39b70740420aa2f1d00
TreasureUzoma <treasureuzoma650@gmail.com>
2025-04-17 12:55:01 +0100

    ui tweaks

--- kairos-landing/app/layout.tsx
+++ kairos-landing/app/layout.tsx
@@ -1,21 +1,33 @@
 import type { Metadata } from "next";
 import "./globals.css";
+
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"]
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"]
+});
+
 export const metadata: Metadata = {
   title: "Kairos - Keep Streaks",
   description:
     "Kairos is a intuitive dev community that allows you create project, keep streaks, get reviews with a leaderboard feature.",
-  keywords:
-    "kairos, streak, 30 days challenge, #30daysofcide, reddit, vickyjaychallenge",
+  keywords: "kairos, streak, 30 days challenge, #30daysofcide, reddit, vickyjaychallenge"
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- kairos-landing/components/Nav.tsx
+++ kairos-landing/components/Nav.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { IoMdMenu } from "react-icons/io";
 import { IoMdClose } from "react-icons/io";
+import Link from "next/link";
 import Button from "./props/Button";
 type NavItem = {
   name: string;
@@ -11,8 +12,9 @@ type NavItem = {
 
 const navigator: NavItem[] = [
   { name: "Home", link: "/" },
-  { name: "Fqr", link: "/" },
-  { name: "Teams", link: "/" },
+  { name: "About", link: "/#about" },
+  { name: "Faq", link: "/#faqs" },
+  { name: "Teams", link: "/#teams" }
 ];
 export function DesktopNav() {
   const [toggle, setToggle] = useState<boolean>(false);
@@ -21,35 +23,24 @@ export function DesktopNav() {
   };
 
   return (
-    <main>
+    <div>
       <section className="relative z-0 ">
         <nav className="text-white flex justify-between py-2 md:py-4  bg-black/40 backdrop-blur-md fixed  top-0 right-0 left-0 items-center px-3 md:px-12 ">
           <div className="flex  items-center space-x-2">
-            <Image
-              src="/white-logo.png"
-              alt="Logo"
-              width={29}
-              height={29}
-              className="object-contain"
-            />
-            <p className=" text-[22px] font-orbitron md:text-[20px] font-bolder ">
-              Kairos
-            </p>
+            <Link href="/">
+              <Img src={"/logo.svg"} className="h-7.5 dark:invert w-fit" alt="logo" />
+            </Link>
+            <p className=" text-[22px] font-orbitron md:text-[20px] font-bolder ">Kairos</p>
           </div>
           <ul className="md:flex items-center space-x-5 hidden">
-             {navigator.map((items, index) => (
+            {navigator.map((items, index) => (
               <li key={index}>{items.name}</li>
             ))}
           </ul>
-          <div className="space-x-2 md:flex items-center  hidden ">
-            <Button
-              className="border-[#872341] border  px-8 py-2 rounded-lg"
-              name="Sign in"
-            />
-            <Button
-              className="bg-[#872341] px-8 py-2 rounded-lg"
-              name="Get started"
-            />
+          <div className="space-x-2 md:flex items-center hidden ">
+            <Link href="/auth">
+              <Button className="bg-[#872341] px-8 py-2 rounded-lg" name="Get started" />
+            </Link>
           </div>
           <div className="md:hidden" onClick={Toggleme}>
             {toggle ? <IoMdClose size={30} /> : <IoMdMenu size={30} />}
@@ -57,14 +48,14 @@ export function DesktopNav() {
         </nav>
       </section>
       {toggle && <MobileNav />}
-    </main>
+    </div>
   );
 }
 
 function MobileNav() {
   return (
-    <main className="">
-      <nav className="bg-black/40  backdrop-blur-md grid grid-cols-3 top-14 mt-5  mx-3 fixed  left-0 right-0 py-4 rounded-2x     px-3 my-2 rounded-lg">
+    <div>
+      <nav className="bg-black/40  backdrop-blur-md grid grid-cols-3 top-14 mt-5  mx-3 fixed left-0 right-0 py-5 rounded-2x px-3 my-2 rounded-lg">
         <aside className="text-white">
           <ul className="space-y-8">
             {navigator.map((items, index) => (
@@ -74,15 +65,12 @@ function MobileNav() {
         </aside>
         <article className="col-span-2">
           <div className="space-x-2 text-white flex flex-col gap-y-4">
-            <button className="border-[#872341] border text-[13px]  px-2 py-2 rounded-lg">
-              Sign in
-            </button>
-            <button className="bg-[#872341] px-8 py-2 text-[13px] rounded-lg">
+            <Link href="/auth" className="bg-[#872341] px-8 py-2 text-[13px] rounded-lg">
               Get started
-            </button>
+            </Link>
           </div>
         </article>
       </nav>
-    </main>
+    </div>
   );
 }
